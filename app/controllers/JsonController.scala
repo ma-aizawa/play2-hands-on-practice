94f80e34a9df4b4121c0078d769c7e17e2b8c97d
masahiro aizawa <kingyodojo@gmail.com>
2016-01-01 02:14:53 +0900

    Implement JSON API for create / update
    https://github.com/bizreach/play2-hands-on/blob/master/play2.4-slick3.0/markdown/11_implement_update_api.md

--- app/controllers/JsonController.scala
+++ app/controllers/JsonController.scala
@@ -16,10 +16,40 @@ import play.api.libs.functional.syntax._
 object JsonController {
   // UsersRowをJSONに変換するためのWritesを定義
   implicit val usersRowWritesWrites = (
-    (__ \ "id" ).write[Long] and
-    (__ \ "name" ).write[String] and
-    (__ \ "companyId" ).writeNullable[Int]
+    (__ \ "id").write[Long] and
+    (__ \ "name").write[String] and
+    (__ \ "companyId").writeNullable[Int]
   )(unlift(UsersRow.unapply))
+
+  // ユーザー情報のためのケースクラス
+  case class UserForm(id: Option[Long], name: String, companyId: Option[Int])
+
+  // JSONをUserFormに変換するためのReadsを定義
+  implicit val userFormFormat = (
+    (__ \ "id").readNullable[Long] and
+    (__ \ "name").read[String] and
+    (__ \ "companyId").readNullable[Int]
+  )(UserForm)
+
+  /* JSON DSFを使わない変換
+  implict val userFormFormat = new Reads[UserForm]{
+    def read(js: JsValue): UserForm = {
+      UserForm(
+        id = (js \ "id").asOpt[Long],
+        name = (js \ "name").as[String],
+        companyId = (js \ "companyId").asOpt[Int]
+      )
+    }
+  }
+  */
+
+  /* マクロを使って定義
+  implicit val userFormReads = Json.reads[UserForm]
+  implicit val userFormWrites = Json.writes[UserForm]
+  */
+  /* マクロを使ってWritesとReadsをまとめて定義
+  implicit val userFormFormat = Json.format[UserForm]
+  */
 }
 
 class JsonController @Inject()(val dbConfigProvider: DatabaseConfigProvider) extends Controller
@@ -33,9 +63,31 @@ class JsonController @Inject()(val dbConfigProvider: DatabaseConfigProvider) ext
     }
   }
 
-  def create = TODO
+  def create = Action.async(parse.json) { implicit rs =>
+    rs.body.validate[UserForm].map { form =>
+      val user = UsersRow(0, form.name, form.companyId)
+      db.run(Users += user).map { _ =>
+        Ok(Json.obj("result" -> "success"))
+      }
+    }.recoverTotal { e =>
+      Future {
+        BadRequest(Json.obj("result" -> "failure", "error" -> JsError.toJson(e)))
+      }
+    }
+  }
 
-  def update = TODO
+  def update = Action.async(parse.json) { implicit rs =>
+    rs.body.validate[UserForm].map { form =>
+      val user = UsersRow(form.id.get, form.name, form.companyId)
+      db.run(Users.filter(t => t.id === user.id.bind).update(user)).map { _ =>
+        Ok(Json.obj("result" -> "success"))
+      }
+    }.recoverTotal {e =>
+      Future {
+        BadRequest(Json.obj("result" -> "failure", "error" -> JsError.toJson(e)))
+      }
+    }
+  }
 
   def remove(id: Long) = TODO
 }
