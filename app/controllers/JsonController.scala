6ca505638c80e8a05eccd26d8c466bdd7dcaf26b
masahiro aizawa <kingyodojo@gmail.com>
2016-01-01 01:43:29 +0900

    Implement json list action
    https://github.com/bizreach/play2-hands-on/blob/master/play2.4-slick3.0/markdown/10_implement_list_api.md

--- app/controllers/JsonController.scala
+++ app/controllers/JsonController.scala
@@ -13,10 +13,25 @@ import slick.driver.H2Driver.api._
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
+object JsonController {
+  // UsersRowをJSONに変換するためのWritesを定義
+  implicit val usersRowWritesWrites = (
+    (__ \ "id" ).write[Long] and
+    (__ \ "name" ).write[String] and
+    (__ \ "companyId" ).writeNullable[Int]
+  )(unlift(UsersRow.unapply))
+}
+
 class JsonController @Inject()(val dbConfigProvider: DatabaseConfigProvider) extends Controller
     with HasDatabaseConfigProvider[JdbcProfile] {
+  import JsonController._
 
-  def list = TODO
+  def list = Action.async { implicit rs =>
+    // ID昇順に全て取得
+    db.run(Users.sortBy(t => t.id).result).map { users =>
+      Ok(Json.obj("users" -> users))
+    }
+  }
 
   def create = TODO
 
