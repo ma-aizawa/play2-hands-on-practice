13442b4d55960e6d92e5742b5adfb04a26dc39eb
masahiro aizawa <kingyodojo@gmail.com>
2015-12-31 23:18:05 +0900

    play2-hands-onをやってみている
    https://github.com/bizreach/play2-hands-on/blob/master/play2.4-slick3.0/markdown/04_define_routing.md
    まで実装

---
+++ app/controllers/UserController.scala
@@ -0,0 +1,29 @@
+package controllers
+
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.i18n.{MessagesApi, I18nSupport}
+import play.api.db.slick._
+import slick.driver.JdbcProfile
+import models.Tables._
+import javax.inject.Inject
+import scala.concurrent.Future
+import slick.driver.H2Driver.api._
+
+class UserController @Inject() (val dbConfigProvicer: DatabaseConfigProvider,
+                                val messagesApi: MessagesApi) extends Controller
+    with HasDatabaseConfigProvider[JdbcProfile] with I18nSupport {
+
+  def list = TODO
+
+  def edit(id: Option[Long]) = TODO
+
+  def create = TODO
+
+  def update = TODO
+
+  def remove(id: Long) = TODO
+}
+
