c611a18a09352332727a9c4375b446d95640eefe
masahiro aizawa <kingyodojo@gmail.com>
2016-01-01 00:12:44 +0900

    新規作成 / 編集 画面を作成
    https://github.com/bizreach/play2-hands-on/blob/master/play2.4-slick3.0/markdown/06_implement_user_form.md

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -12,9 +12,24 @@ import javax.inject.Inject
 import scala.concurrent.Future
 import slick.driver.H2Driver.api._
 
+object UserController {
+  // フォームの値を格納するケースクラス
+  case class UserForm(id: Option[Long], name: String, companyId: Option[Int])
+
+  // formから送信されたデータ <=> ケースクラスの変換を行う
+  val userForm = Form(
+    mapping(
+      "id" -> optional(longNumber),
+      "name" -> nonEmptyText(maxLength = 20),
+      "companyId" -> optional(number)
+    )(UserForm.apply)(UserForm.unapply)
+  )
+}
+
 class UserController @Inject() (val dbConfigProvider: DatabaseConfigProvider,
                                 val messagesApi: MessagesApi) extends Controller
     with HasDatabaseConfigProvider[JdbcProfile] with I18nSupport {
+  import UserController._
 
   def list = Action.async { implicit rs =>
     // IDの昇順に全てのユーザ情報を取得
@@ -23,7 +38,23 @@ class UserController @Inject() (val dbConfigProvider: DatabaseConfigProvider,
     }
   }
 
-  def edit(id: Option[Long]) = TODO
+  def edit(id: Option[Long]) = Action.async { implicit rs =>
+    // リクエストにIDがある
+    val form = if(id.isDefined) {
+      db.run(Users.filter(t => t.id === id.get.bind).result.head).map { user =>
+        userForm.fill(UserForm(Some(user.id), user.name, user.companyId))
+      }
+    } else {
+      Future { userForm }
+    }
+
+    form.flatMap { form =>
+      // 会社一覧
+      db.run(Companies.sortBy(_.id).result).map { companies =>
+        Ok(views.html.user.edit(form, companies))
+      }
+    }
+  }
 
   def create = TODO
 
